34641213e2af05834a7f88dcce1e8644439c9bc5
ajzajac <ajzajac@icloud.com>
2020-11-07 23:03:29 -0500

    minor format space added

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Details(props) {
     return (
         <div className='detailsList'>
-            <div><p><b>Current Weather:</b>  {props.description}</p></div>
-            <div><p><b>Feels like:</b>  {props.feelsLike ? props.feelsLike.toFixed(0) : null}ºF</p></div>
-            <div><p><b>Today's High:</b>  {props.highTemp ? props.highTemp.toFixed(0) : null}ºF</p></div>
-            <div><p><b>Today's Low:</b>  {props.lowTemp ? props.lowTemp.toFixed(0) : null}ºF</p></div>
-            <div><p><b>Sunrise:</b>  {props.sunrise}</p></div>
-            <div><p><b>Sunset:</b>  {props.sunset}</p></div>
-            <div><p><b>Humidity:</b>  {props.humidity}%</p></div>
+            <div><p><b>Current Weather: </b>  {props.description}</p></div>
+            <div><p><b>Feels like: </b>  {props.feelsLike ? props.feelsLike.toFixed(0) : null}ºF</p></div>
+            <div><p><b>Today's High: </b>  {props.highTemp ? props.highTemp.toFixed(0) : null}ºF</p></div>
+            <div><p><b>Today's Low: </b>  {props.lowTemp ? props.lowTemp.toFixed(0) : null}ºF</p></div>
+            <div><p><b>Sunrise: </b>  {props.sunrise}</p></div>
+            <div><p><b>Sunset: </b>  {props.sunset}</p></div>
+            <div><p><b>Humidity: </b>  {props.humidity}%</p></div>
         </div>
     )
 }
